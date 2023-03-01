7851e7fef3b219f77d08fa50cdf29de1e7f1d427
Rafael Idrisov <rafael.idrisov@gmail.com>
2023-03-02 02:00:02 +0300

    Home PC 02.03.2023 01:59

--- pgx03/entries.go
+++ pgx03/entries.go
@@ -1,4 +1,4 @@
-package entries
+package main
 
 type Transaction struct {
 	account       string
@@ -6,7 +6,7 @@ type Transaction struct {
 	trtype        string
 	docdate       string
 	docnumb       string
-	counterpary   string
+	counterparty  string
 	cntp_tax_id   string
 	cntp_contract string
 	purpose       string
--- pgx03/main.go
+++ pgx03/main.go
@@ -11,13 +11,8 @@ import (
 
 func main() {
 	log.Println("starting program")
-	// get the database connection URL.
-	// usually, this is taken as an environment variable as in below commented out code
-	// databaseUrl = os.Getenv("DATABASE_URL")
-	// for the time being, let's hard code it as follows. change the values as needed.
-	// databaseUrl := "postgres://postgres:mypassword@localhost:5432/postgres"
 
-	databaseUrl := "postgres://postgres:qwq121@localhost:5432/postgres"
+	databaseUrl := "postgres://postgres:qwq121@localhost:5432/budgeting"
 	dbPool, err := pgxpool.Connect(context.Background(), databaseUrl)
 
 	if err != nil {
@@ -26,4 +21,50 @@ func main() {
 	}
 	//to close DB pool
 	defer dbPool.Close()
+}
+
+func AddTransaction(dpPool *pgxpool.Pool) {
+	log.Println("adding new entry")
+	entry := &Transaction{}
+
+	fmt.Printf("Account: ")
+	fmt.Scanf("%s", entry.account)
+
+	fmt.Printf("Date of transaction: ")
+	fmt.Scanf("%s", entry.trdate)
+
+	fmt.Printf("Type of document: ")
+	fmt.Scanf("%s", entry.trtype)
+
+	fmt.Printf("Date of document: ")
+	fmt.Scanf("%s", entry.docdate)
+
+	fmt.Printf("Number of document: ")
+	fmt.Scanf("%s", entry.docnum)
+
+	fmt.Printf("Counterparty: ")
+	fmt.Scanf("%s", entry.counterparty)
+
+	fmt.Printf("Tax ID of counterparty: ")
+	fmt.Scanf("%s", entry.cntp_tax_id)
+
+	fmt.Printf("Contract: ")
+	fmt.Scanf("%s", entry.cntp_contract)
+
+	fmt.Printf("Purpose of payment: ")
+	fmt.Scanf("%s", entry.purpose)
+
+	fmt.Printf("Direction (inflow/outflow): ")
+	fmt.Scanf("%s", entry.direction)
+
+	fmt.Printf("Amount: ")
+	fmt.Scanf("%.2f", entry.amount)
+
+	fmt.Printf("Item: ")
+	fmt.Scanf("%s", entry.item)
+
+	fmt.Printf("Comment: ")
+	fmt.Scanf("%s", entry.comment)
+
+	queryText := fmt.Sprintf("INSERT INTO transactions(account, trdate, trtype, docdate, docnumb, counterpary, cntp_tax_id, cntp_contract, purpose, comment, direction, amount, item) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %.2f, %s)")
 }
