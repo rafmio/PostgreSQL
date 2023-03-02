19ce12f8760617a02d17b8a2a88eac5e931a0396
Rafael Idrisov <rafael.idrisov@gmail.com>
2023-03-03 00:00:11 +0300

    Home PC 03.03.2023 00:00

--- pgx03/entries.go
+++ pgx03/entries.go
@@ -1,65 +1,65 @@
 package main
 
-import (
-	"reflect"
-	"log"
-)
-
 // Структура транзакции
 type Transaction struct {
-	Account       string
-	TrDate        string
-	TrType        string
-	DocDate       string
-	DocNumb       string
-	Counterparty  string
-	CntpTaxID   string
-	Contract  string
-	Purpose       string
-	Direction     string
-	Amount        float64
-	Item          string
-	Comment       string
+	Account      string
+	TrDate       string
+	TrType       string
+	DocDate      string
+	DocNumb      string
+	Counterparty string
+	CntpTaxID    string
+	Contract     string
+	Purpose      string
+	Direction    string
+	Amount       float64
+	Item         string
+	Comment      string
 }
 
 // Описание каждого поля структуры транзакции для вывода
-var TransactionFieldsDescription map[string]string = map[string]string {
-	"Account" : "Account",
-	"TrDate": "Date of transaction",
-	"TrType": "Type of transaction",
-	"DocDate": "Date of document",
-	"DocNumb": "Number of document",
+var TransactionFieldsDescription map[string]string = map[string]string{
+	"Account":      "Account",
+	"TrDate":       "Date of transaction",
+	"TrType":       "Type of transaction",
+	"DocDate":      "Date of document",
+	"DocNumb":      "Number of document",
 	"Counterparty": "Counterparty",
-	"CntpTaxID": "Tax ID of counterparty",
-	"Countract": "Countract",
-	"Purpose": "Purpose of payment",
-	"Direction": "Direction of payment (inflow/outflow)",
-	"Amount": "Amount of payment (roubles)",
-	"Item": "Item of cost",
-	"Comment": "Comment",
+	"CntpTaxID":    "Tax ID of counterparty",
+	"Countract":    "Countract",
+	"Purpose":      "Purpose of payment",
+	"Direction":    "Direction of payment (inflow/outflow)",
+	"Amount":       "Amount of payment (roubles)",
+	"Item":         "Item of cost",
+	"Comment":      "Comment",
 }
 
-// Атомарная функция добавления транзации со всеми полями (не по отдельным полям)
-func (transaction *Transaction) AddFullTransaction() err error {
-	log.Println("adding full transaction")
-
-	values := reflect.ValueOf(transaction)
-	types := values.Type()
-
-// Цикл заполнения структуры
-	for i := 0; i < values.NumField(); i++ {
-		fmt.Printf(TransactionFieldsDescription[types.Field(i).Name], ": ")
-
-		// Значение перебираемого поля помещаем для удобства в отдельную
-		// переменную, чтоб не вызывать лишний раз функции
-		fieldValue := types.Field(i).Name
-
-		// Все поля - string, кроме поля суммы (Amount float64)
-		// Для поля Amount float64 отдельное сканирование с
-		if fieldValue != "Amount" {
-			fmt.Scanf("%s", transaction.fieldValue)
-		} else {
-			fmt.Scanf("%.2f", transaction.fieldValue)
-		}
-	}
+type SetTransaction interface {
+	SetAccount()
+	SetTrDate()
+	SetTrType()
+	SetDocDate()
+	SetDocNumb()
+	SetCounterparty()
+	SetCntpTaxID()
+	SetContract()
+	SetPurpose()
+	SetDirection()
+	SetAmount()
+	SetItem()
+	SetComment()
 }
+
+func (transaction *Transaction) SetAccount() {
+	log.Println("setting ", TransactionFieldsDescription["Account"])
+	fmt.Printf(TransactionFieldsDescription["Account"], ": ")
+	fmt.Scanf("%s", transaction.Account)
+}
+
+func (transaction *Transaction) SetTrDate() {
+	log.Println("settig ", TransactionFieldsDescription["TrDate"])
+	fmt.Printf(TransactionFieldsDescription["TrDate"], ": ")
+	fmt.Scanf("%s", transaction.TrDate)
+}
+
+func 
